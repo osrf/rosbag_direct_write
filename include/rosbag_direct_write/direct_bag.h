856d50ffffc80d346469cc4d1f25aa3dc7e58a00
William Woodall <william@osrfoundation.org>
2015-01-20 19:41:40 -0800

    add basic documentation to the API

--- include/rosbag_direct_write/direct_bag.h
+++ include/rosbag_direct_write/direct_bag.h
@@ -28,24 +28,35 @@ namespace rosbag_direct_write
 // Defined in dependencies header and is a shared_ptr from either boost or std
 using rosbag_direct_write::shared_ptr;
 
+/// This is a uint8_t based, 4096 aligned std::vector container
 typedef std::vector<uint8_t, aligned_allocator<uint8_t, 4096>> VectorBuffer;
 
+/// This DirectFile class provides a simplified interface for writing to files
+/// using the O_DIRECT (or F_NOCACHE) flag for high performance writing.
 class ROSBAG_DIRECT_WRITE_DECL DirectFile
 {
 public:
+  /// Opens the given file with O_DIRECT, overwriting an existing file.
   DirectFile(std::string filename);
   ~DirectFile();
 
+  /// Returns the cursor location as a number of bytes from the file start.
   size_t get_offset() const;
+  /// Moves the cursor location to the given position.
   void seek(uint64_t pos) const;
 
+  /// Writes the given VectorBuffer to the file.
   size_t write_buffer(VectorBuffer &buffer);
+  /// Writes the given uint8_t array for given length bytes to the file.
   size_t write_data(const uint8_t *start, size_t length);
 
+  /// Returns the current file's name.
   std::string get_filename() {return this->filename_;}
 
+  /// Returns true if the file is open, otherwise false.
   bool is_open();
 
+  /// Closes the file, flushing the output.
   void close();
 
 private:
@@ -59,16 +70,31 @@ private:
 
 };
 
+/// The DirectBag class provides an interface for writing ROS messages or
+/// serializable data structures to a ROS bag file using O_DIRECT.
 class ROSBAG_DIRECT_WRITE_DECL DirectBag
 {
 public:
+  /// Creates a new ROS bag file, overwriting an existing one.
   DirectBag(std::string filename);
+  /// Creates an uninitialized DirectBag; open must be called before use.
   DirectBag();
-
   ~DirectBag();
 
+  /// Opens a ROS bag file at the given location for writing; overwrites.
   void open(std::string filename);
 
+  /// Writes a given data structure to the bag file.
+  /**
+   * The write function takes a topic to write to, a time that is was received,
+   * a shared pointer to the data structure, and an optional connection_header.
+   *
+   * @param topic The topic to write the message to in the bag file.
+   * @param time The received time of the message (used for playback).
+   * @param msg A shared pointer to the message object to serialize and write.
+   * @param connection_header Optional ROS connection header.
+   *
+   */
   template<class T>
   void write(std::string const& topic,
              ros::Time const& time,
@@ -78,6 +104,7 @@ public:
     write(topic, time, *msg, connection_header);
   }
 
+  /// Writes a given data structure to the bag file.
   template<class T>
   void write(std::string const& topic,
              ros::Time const& time,
@@ -87,13 +114,16 @@ public:
     write(topic, time, *msg, connection_header);
   }
 
+  /// Writes a given data structure to the bag file.
   template<class T>
   void write(std::string const& topic,
              ros::Time const& time,
              T const& msg,
              shared_ptr<ros::M_string> connection_header = 0);
 
+  /// Closes the current file, finishing the file with indexing and flushing.
   void close();
+  /// Returns true if the file is open, otherwise false.
   bool is_open() const;
 
 private:
