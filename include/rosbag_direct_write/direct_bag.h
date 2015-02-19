8eab84812873f9ea069822d781b588909717f888
William Woodall <william@osrfoundation.org>
2015-02-19 15:07:16 -0800

    add DirectBagCollection which splits large bags

--- include/rosbag_direct_write/direct_bag.h
+++ include/rosbag_direct_write/direct_bag.h
@@ -17,7 +17,9 @@
 #define ROSBAG_BAG_DIRECT_BAG_DIRECT_H
 
 #include <atomic>
+#include <cmath>
 #include <memory>
+#include <string>
 #include <rosbag_direct_write/direct_bag_dependencies.h>
 
 namespace rosbag_direct_write
@@ -26,7 +28,13 @@ namespace rosbag_direct_write
 #include "aligned_allocator.h"
 
 // Default chunk size.
-static const size_t kdefault_chunk_threshold = 768 * 1024;  // 768kb
+static const size_t kdefault_chunk_threshold = 768 * 1024;  // 768KB
+// Default file prefix used in open_directory
+static const std::string kdefault_file_prefix = "";  // No prefix
+// Default bag size limit in bytes, 2GB is ~73MB less than 2GiB
+static const size_t kdefault_bag_size_threshold = std::pow(10, 9);  // 2GB
+// Default width of bag number suffix
+static const size_t kdefault_bag_number_width = 4;
 
 // Defined in dependencies header and is a shared_ptr from either boost or std
 using rosbag_direct_write::shared_ptr;
@@ -36,6 +44,7 @@ typedef std::vector<uint8_t, aligned_allocator<uint8_t, 4096>> VectorBuffer;
 
 /// This DirectFile class provides a simplified interface for writing to files
 /// using the O_DIRECT (or F_NOCACHE) flag for high performance writing.
+/** Not thread-safe */
 class ROSBAG_DIRECT_WRITE_DECL DirectFile
 {
 public:
@@ -47,6 +56,8 @@ public:
   size_t get_offset() const;
   /// Moves the cursor location to the given position.
   void seek(uint64_t pos) const;
+  /// Returns the current total size of the file.
+  size_t get_size() const;
 
   /// Writes the given VectorBuffer to the file.
   size_t write_buffer(VectorBuffer &buffer);
@@ -75,6 +86,7 @@ private:
 
 /// The DirectBag class provides an interface for writing ROS messages or
 /// serializable data structures to a ROS bag file using O_DIRECT.
+/** Not thread-safe */
 class ROSBAG_DIRECT_WRITE_DECL DirectBag
 {
 public:
@@ -147,6 +159,7 @@ public:
              shared_ptr<ros::M_string> connection_header = 0);
 
   /// Closes the current file, finishing the file with indexing and flushing.
+  /** Thread-safe with concurrent calls to close only (not with write) */
   void close();
   /// Returns true if the file is open, otherwise false.
   bool is_open() const;
@@ -154,6 +167,10 @@ public:
   size_t get_chunk_threshold() const;
   /// Returns the bag file name, returns "" if the bag is not open.
   std::string get_bag_file_name() const;
+  /// Returns the current size of the bag file in bytes.
+  size_t get_bag_file_size() const;
+  /// Returns the current size of the bag file plus any as yet unwritten bytes.
+  size_t get_virtual_bag_size() const;
 
 private:
   template<class T> shared_ptr<rosbag::ConnectionInfo>
@@ -187,6 +204,114 @@ private:
   size_t chunk_threshold_;  // Size at which chunks are ended
 
   uint32_t next_conn_id_;
+};
+
+/// Provides an interface for writing to multiple bag files which are split at
+/// a given size threshold.
+/** Not thread-safe. */
+class DirectBagCollection
+{
+public:
+  /// DirectBagCollection instances always start "closed".
+  DirectBagCollection();
+  ~DirectBagCollection();
+
+  /// Creates potentially many bag files in a given directory, splitting them
+  /// at the bag_size_threshold.
+  /**
+   * Writes one or more bag files in the given directory.
+   *
+   * The bag file names are created by concatenating the given file_prefix and
+   * the number of each bag in the series with a hyphen. However, the hyphen
+   * is dropped if file_prefix is an empty string. For example, if the
+   * file_prefix is "foo" and three bags are created then they would be:
+   *
+   *  - foo-0001.bag
+   *  - foo-0002.bag
+   *  - foo-0003.bag
+   *
+   * If the file_prefix is "" and there are two bags:
+   *
+   *  - 0001.bag
+   *  - 0002.bag
+   *
+   * The fixed width precision of the number is 4 by default, but can be
+   * adjusted using the bag_number_width.
+   *
+   * Existing files will be overwritten, so care should be taken that files
+   * go together. For example, if you call open_directory on two different
+   * occasions where the first occasion generated more files you may end up
+   * with something like this:
+   *
+   *  - foo-0001.bag - overwritten in second run
+   *  - foo-0002.bag - overwritten in second run
+   *  - foo-0003.bag - from the first run, not part of second run
+   *
+   * The behavior of this mode is that a file is opened when needed, and if
+   * it existed before its content is removed before new data is written.
+   * Nothing more intelligent can be assumed about this mode of operation.
+   *
+   * Bag files are split at or over the given bag_size_threshold, i.e. bag
+   * files can exceed the given limit.
+   *
+   * See the documentation on open for information about how chunk_threshold
+   * is used.
+   *
+   * @param folder_path The path of the folder to which ROS bag files should
+   *                    be written.
+   * @param chunk_threshold The size at which chunks are closed,
+   *                        defaults to 768kb.
+   * @param file_prefix Prefix for each bag file created, defaults to ""
+   * @param bag_size_threshold Size in bytes at which bag files should be
+   *                           split up, defaults to 2GB (10^9 bytes).
+   * @param bag_number_width Fixed width precision of the number suffix for
+   *                         bags, defaults to 4, e.g. <file_prefix>-000N.bag.
+   *
+   * @throw std::invalid_argument If the bag_number_width is zero.
+   */
+  void open_directory(std::string folder_path,
+                      std::string file_prefix=kdefault_file_prefix,
+                      size_t chunk_threshold=kdefault_chunk_threshold,
+                      size_t bag_size_threshold=kdefault_bag_size_threshold,
+                      size_t bag_number_width=kdefault_bag_number_width);
+
+  /// Closes the currently open bag and returns a list of all bags written.
+  /** Thread-safe with concurrent calls to close only (not with write) */
+  std::vector<std::string> close();
+
+  /// Forwards arguments to one of the write functions for the open rosbag.
+  template<typename ...Args>
+  void write(Args && ...args);
+
+  /// Returns true if a file is open, otherwise false.
+  bool is_open() const;
+  /// Returns the directory to which bag files are being written.
+  std::string get_folder_path() const;
+  /// Returns the size, in bytes, of the chunk threshold, can throw
+  /// std::runtime_error if closed.
+  size_t get_chunk_threshold() const;
+  /// Returns the size, in bytes, of the bag file threshold.
+  size_t get_bag_size_threshold() const;
+  /// Returns the number of the current bag (1 indexed, 0 if closed).
+  size_t get_current_bag_number() const;
+  /// Returns the file name of the current bag or throws
+  /// std::runtime_error if closed.
+  std::string get_current_bag_file_name() const;
+
+private:
+  void check_bag_threshold_();
+  void close_current_bag_();
+  void open_next_bag_();
+
+  std::string folder_path_;
+  std::string file_prefix_;
+  size_t chunk_threshold_;
+  size_t bag_size_threshold_;
+  size_t bag_number_width_;
+
+  std::atomic<bool> open_;
+  size_t current_bag_number_;
+  shared_ptr<DirectBag> current_bag_;
 };
 
 /// Macro used to prevent compiler warnings, should optimize out.
